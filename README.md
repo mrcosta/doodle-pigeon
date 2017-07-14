444e8c43e9b5edaa69824882bc1b3ebca333359d
Mateus Costa <mateuscomp@gmail.com>
2017-07-14 13:39:24 -0300

    add docker-compose to run tests on travis_ci

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # doodle-pigeon
+
+```bash
+docker build -t="mrcosta/doodle-pigeon" .
+```
+
+to run is necessary first to build the application using the build image
+and then build the image to run the application.
+
+```bash
+docker run -p8080:8080 --rm -it mrcosta/doodle-pigeon
+```
+
+docker-compose run test ./gradlew test
--- src/main/java/com/doodle/pigeon/configuration/AppConfig.java
+++ src/main/java/com/doodle/pigeon/configuration/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
-    RequestInterceptor requestInterceptor;
+    private RequestInterceptor requestInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
